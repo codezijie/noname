a50352bb9150227c1fe47ddef286d6e26e50a2ce
Jebearssica <Jebearssica@outlook.com>
2023-06-09 20:24:11 +0800

    [feat]: basic operator for Point

--- include/geometry/Point.h
+++ include/geometry/Point.h
@@ -1,19 +1,36 @@
+#ifndef HDR_POINT
+#define HDR_POINT
 
 namespace geometry
 {
     /**
      *  @brief A Point represents a coordinate in 2D space.
-     *  
+     *
      *  @note The coordinate should be integer, which means all the points should be snapped at input.
      */
     class Point
     {
     private:
         int m_x, m_y;
+
     public:
-        // TODO: all the involved Points should be normalized in Polygon, so override operator< is necessary.
-        Point(/* args */);
+        Point();
+        Point(int x, int y);
+        Point(const Point &p);
         ~Point();
+
+        void swap(Point &p);
+
+        Point &operator+=(const Point &p);
+        Point &operator-=(const Point &p);
+
+        bool operator<(const Point &p) const;
+        bool operator==(const Point &p) const;
+        bool operator!=(const Point &p) const;
     };
-    
+
+    Point &operator+(const Point &p1, const Point &p2);
+    Point &operator+(const Point &p1, const Point &p2);
 } // namespace geometry
+
+#endif
